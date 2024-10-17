0e9d6d4cd282d2a72a23f90cebf843b7177c857b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-10-17 14:03:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="midplot",
-    version="0.0.1",
+    version="0.0.2",
     description="Plotting margingales",
     long_description=README,
     long_description_content_type="text/markdown",
